84a70b9136fd8b27f96b2e65df2161f3f2906bda
Zach Breit <zevwolf9@yahoo.com>
2017-11-30 19:09:36 +0000

    Clarified comments in ternary.cpp

--- miscellaneous/ternary.cpp
+++ miscellaneous/ternary.cpp
@@ -6,34 +6,45 @@ int main()
 {
     //ternary operator (condition) ? true : false
     int num = (1==1) ? 0 : -1;
-    if (1 == 1)
-    {
-        int num = 0;
-    }
-    else
-    {
-        int num = -1;
-    }
+    /*
+        This is the equivalent if/else statement:
+        if (1 == 1)
+        {
+            int num = 0;
+        }
+        else
+        {
+            int num = -1;
+        }
+    */
+    
     // num is 0, 1 is equal to 1
     int newnum = (num == 1) ? 7 : 5;
-    if (num == 1)
-    {
-        int newnum = 7;
-    }
-    else
-    {
-        int newnum = 5;
-    }
+    /*
+        This is the equivalent if/else statement:
+        if (num == 1)
+        {
+            int newnum = 7;
+        }
+        else
+        {
+            int newnum = 5;
+        }
+    */
+    
     // set newnum to 5, 0 != 1
     cout << ((newnum > 6) ? "Hello" : "Goodbye") << endl; // ternary operators within functions
-    if (newnum > 6)
-    {
-        cout << "Hello" << endl;
-    }
-    else
-    {
-        cout << "Goodbye" << endl;
-    }
+    /*
+        This is the equivalent statement:
+        if (newnum > 6)
+        {
+            cout << "Hello" << endl;
+        }
+        else
+        {
+            cout << "Goodbye" << endl;
+        }
+    */
     // newnum is 5 which is less than 6 so prints goodbye
     
     // cout << (newnum > 6) ? "Hello" : "Goodbye" << endl;
