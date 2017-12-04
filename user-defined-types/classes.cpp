fc9075ce5aeae5b54d2573ab097ad07685fb16e4
Zach Breit <zbreit18@gmail.com>
2017-12-04 16:15:40 -0500

    centuries

--- user-defined-types/classes.cpp
+++ user-defined-types/classes.cpp
@@ -8,22 +8,20 @@ class DateClass
         int m_month;
         int m_day;
         int m_year;
-        int m_century;
 
         // You can also member functions (methods)
         void print()
         {
-            cout << m_month << "/" << m_day << "/" << m_century - 1 << m_year << endl;
+            cout << m_month << "/" << m_day << "/" << m_year << endl;
         }
 
         // Create a Constructor
         // This is run when the object is declared and initialized
-        DateClass(int month = 1, int day = 1, int year = 00, int century = 21)
+        DateClass(int month = 1, int day = 1, int year = 2000)
         {
             m_month = month;
             m_day = day;
             m_year = year;
-            m_century = century;
         }
 };
 
@@ -34,14 +32,13 @@ int main()
     // Setting the attributes manually
     today.m_month = 12;
     today.m_day = 4;
-    today.m_year = 17;
-    today.m_century = 21;
+    today.m_year = 2017;
 
     // Run the print method for the today object
     today.print();
 
     // Defining an object "myBirthday" that belongs to the class "DateClass"
-    DateClass myBirthday(8, 20, 99, 20);
+    DateClass myBirthday(8, 20, 1999);
     myBirthday.print();
 
 
