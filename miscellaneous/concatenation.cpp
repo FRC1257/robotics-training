d10663075e8ac83222450126e7e0edb24e6c6312
Zach Breit <zbreit18@gmail.com>
2017-12-07 16:26:53 -0500

    Various changes before monster commit

---
+++ miscellaneous/concatenation.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string first = "Zach";
+    string last = "Breit";
+    cout << "Name = " + first + " " +  last << endl;
+    return 0;
+}
