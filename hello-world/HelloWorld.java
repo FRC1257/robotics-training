f310c0e7bfa8706cc33f880207be8c908c852f3b
Omay Edekar <omayedekar@gmail.com>
2018-05-23 16:18:03 -0400

    Create HelloWorld.java

---
+++ hello-world/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+	  public static void main(String[] args) {
+	        System.out.println("Hello World!");
+	        System.out.println("My name is Omay");
+	        
+	        int number = Integer.parseInt(args[0]);
+	        System.out.println(number);
+	    }
+}
